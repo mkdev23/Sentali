b61efd626275a8cc47f12463e1f4c02c92b7a133
Julius <Julius.Stewart95@gmail.com>
2025-09-10 01:25:18 -0400

    glb loader

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -65,7 +65,7 @@ const options = {
 (async () => {
   try {
     const sb = await loadGLBSkybox(
-  'https://sentalistorage23075.blob.core.windows.net/skyboxes/sentali_skybox.glb?sp=r&st=2025-09-10T04:07:34Z&se=2027-09-11T12:22:34Z&spr=https&sv=2024-11-04&sr=b&sig=VvlNDwJ5iSJGDkIcLcdCsQULT7iLPJbnrIzHVgf4wAg%3D',
+ 'https://sentali-cdn.azurefd.net/skyboxes/sentali_skybox.glb?sp=r&st=2025-09-10T04:07:34Z&se=2027-09-11T12:22:34Z&spr=https&sv=2024-11-04&sr=b&sig=VvlNDwJ5iSJGDkIcLcdCsQULT7iLPJbnrIzHVgf4wAg%3D',
   scene,
   camera,
   options
